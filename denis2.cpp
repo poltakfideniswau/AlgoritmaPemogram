2bf3625bb7a76c64dcb32bcadfe738defd409a5b
poltakfideniswau <wauden3@gmail.com>
2025-04-25 20:28:43 +0700

    Add files via upload

---
+++ denis2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"Masukan kata = Halo"<<endl;
+	cim>>kata;
+	
+	if (kata=="HALO"){
+		cout<<"kata yang dimasukan sesuai"<<endl;
+	}else{
+		cout<<"kata yang dimasukan tidak sesuai"<<endl;
+	}	
+}
+
